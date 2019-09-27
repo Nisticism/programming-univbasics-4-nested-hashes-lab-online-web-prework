78768a712adcf2d80fcc84bb74d6248c54e47552
Hans Foster <fosterhans@gmail.com>
2019-09-27 04:24:18 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,9 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     dennis_language = ""
-     dennis_language = programmer_hash[:dennis_ritchie][:languages]
-     dennis_language
+  programmer_hash[:dennis_ritchie][:languages[0]]
 end
 
 def adding_matz
